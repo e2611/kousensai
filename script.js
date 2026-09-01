e93c9d6941f334278ec2f12e765add0fbb81dda7
e2611 <e2611@s.akashi.ac.jp>
2026-09-01 13:50:22 +0900

    huhu

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+const moreinfobtn = document.getElementById("moreinfobtn");
+moreinfobtn.addEventListener("click",()=>{
+    window.open("https://www.akashi.ac.jp/departments.html","_blank");
+})
