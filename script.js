4d8bc4ff0b0b5850c3b59594eb5151a930502fe5
e2611 <e2611@s.akashi.ac.jp>
2026-09-01 16:53:27 +0900

    gggg

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ starttestbtn.addEventListener("click",()=>{
 fetch("questions.json")
     .then(response => response.json())
   .then(data => {
-    console.log(data.name);
+    const q1Element = document.getElementById("q1");
+    const questionData = data.find(q => q.questionnumber === "q1");
+    if (questionData) {
+      q1Element.textContent = questionData.question;
+    } else {
+      q1Element.textContent="ちょっとやばい"
+    }
   })
   .catch(error => console.error('エラー:', error));
