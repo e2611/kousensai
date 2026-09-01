c27c49134cf3efb9ffecb2f56a131b2ff68fc905
e2611 <e2611@s.akashi.ac.jp>
2026-09-01 16:15:49 +0900

    aaaaaaa

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const moreinfobtn = document.getElementById("moreinfobtn");
 moreinfobtn.addEventListener("click",()=>{
     window.open("https://www.akashi.ac.jp/departments.html","_blank");
 })
+const starttestbtn = document.getElementById("starttestbtn");
+starttestbtn.addEventListener("click",()=>{
+    location.href="test.html";
+})
