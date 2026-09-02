3883017a5b96b13164b9ec2927b67aa50a677101
e2611 <e2611@s.akashi.ac.jp>
2026-09-02 10:25:16 +0900

    sleepy

--- script2.js
+++ script2.js
@@ -1,12 +1,14 @@
+const container =document.getElementById("container");
 fetch("questions.json")
-    .then(response => response.json())
-  .then(data => {
-    const q1Element = document.getElementById("q1");
-    const questionData = data.find(q => q.questionnumber === "q1");
-    if (questionData) {
-      q1Element.textContent = questionData.question;
-    } else {
-      q1Element.textContent="jsonファイルにq1のデータがありません。"
-    }
+  .then(response => response.json)()
+  .then(data =>{
+    data.forEach(question =>{
+      const questioncontainer = document.createElement("div");
+      questioncontainer.id = question.questionnumber;
+      const Newh2 =document.createElement("h2");
+      Newh2.textContent=question.questionnumer;
+      const Newp = document.cretaeElement("p");
+      Newp.textContent = question.question;
+          })
+
   })
-  .catch(error => console.error('エラー:', error));
