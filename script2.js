42bb1d9b9fe05a40cf132d0a77269aee234158f6
e2611 <e2611@s.akashi.ac.jp>
2026-09-02 11:07:12 +0900

    ffffffffffffffff

--- script2.js
+++ script2.js
@@ -13,6 +13,18 @@ fetch("questions.json")
       Newp.textContent = question.question;
       questioncontainer.appendChild(Newh2);
       questioncontainer.appendChild(Newp);
+      question.options.forEach(option =>{
+        const label = document.createElement("label");
+        label.textContent= option.text;
+        label.classList.add("reverse");
+        const input = document.createElement("input");
+        input.type="radio";
+        input.value=option.dep;
+        input.name=question.questionnumber;
+        questioncontainer.appendChild(label);
+        label.appendChild(input);
+
+      })
     });
 
   });
