441bf2823e642d25588a34bb9da3502008f35e0b
e2611 <e2611@s.akashi.ac.jp>
2026-09-02 10:42:13 +0900

    y8y8y8

--- script2.js
+++ script2.js
@@ -1,14 +1,21 @@
-const container =document.getElementById("container");
+const container = document.getElementById("container");
 fetch("questions.json")
-  .then(response => response.json)()
-  .then(data =>{
-    data.forEach(question =>{
+  .then(response => response.json()) 
+  .then(data => {
+    
+    data.forEach(question => {
       const questioncontainer = document.createElement("div");
       questioncontainer.id = question.questionnumber;
-      const Newh2 =document.createElement("h2");
-      Newh2.textContent=question.questionnumer;
-      const Newp = document.cretaeElement("p");
+      container.appendChild(questioncontainer);
+      
+      const Newh2 = document.createElement("h2");
+      Newh2.textContent = question.questionnumber;
+      
+      const Newp = document.createElement("p");
       Newp.textContent = question.question;
-          })
+      
+      questioncontainer.appendChild(Newh2);
+      questioncontainer.appendChild(Newp);
+    });
 
-  })
+  });
