41921b132ab3dbecacea604d2ffb2e477a4ede72
e2611 <e2611@s.akashi.ac.jp>
2026-09-04 15:15:42 +0900

    ss

--- script2.js
+++ script2.js
@@ -81,4 +81,11 @@ resultbtn.addEventListener("click", () => {
   const url = encodeURIComponent('https://e2611.github.io/kousensai/');
   sharex.innerHTML = `<a href="https://twitter.com/intent/tweet?text=${text}&url=${url}" target="_blank" hashtags="明石高専,高専祭">Xでシェア</a>`;
   resultcontainer.appendChild(sharex);
+  const restartbtn = document.createElement("button");
+  restartbtn.textContent="もう一度初めから診断する";
+  resultcontainer.appendChild(restartbtn);
+  restarbtn.addEventListener("click",()=>{
+    location.reload();
+  }
+  )
 });
