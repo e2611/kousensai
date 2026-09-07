76f0dc2ec8707791d35b070b8b63d91045323025
e2611 <e2611@s.akashi.ac.jp>
2026-09-07 12:08:32 +0900

    a

--- script2.js
+++ script2.js
@@ -74,7 +74,7 @@ resultbtn.addEventListener("click", () => {
   if (!resultDisplay) {
   resultDisplay = document.createElement("p");
   resultDisplay.id = "result-display";
-  const buttoncontainer = document.getElementById("buttoncontainer");
+  const buttoncontainer = document.getElementById("b");
   buttoncontainer.appendChild(resultDisplay);
   }
   resultDisplay.textContent = `診断結果：${resultMessages[resultType] }`;
